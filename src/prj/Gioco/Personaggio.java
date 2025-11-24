8d83ceb9511a684c16f218b275353b3c85b0d583
giorgia.diserio <giorgia.diserio@AULA75-PC09.itib.local>
2025-11-24 09:35:45 +0100

    aggiunta classe perzonaggio

---
+++ src/prj/Gioco/Personaggio.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prj.Gioco;
+
+/**
+ *
+ * @author giorgia.diserio
+ */
+public class Personaggio {
+    private String nome;
+    private int hp;
+    private int atk;
+    private int def;
+    private double range;
+    
+}
--- src/prj/Gioco/PrjGioco.java
+++ src/prj/Gioco/PrjGioco.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package prj.ciao;
+package prj.Gioco;
 
 /**
  *
  * @author giorgia.diserio
  */
-public class PrjCiao {
+public class PrjGioco {
 
     /**
      * @param args the command line arguments
