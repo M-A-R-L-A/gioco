85048df610a410ac4957ff996baf09a377a8e3e5
giorgia.diserio <giorgia.diserio@AULA75-PC09.itib.local>
2025-11-24 10:55:26 +0100

    gioco

--- src/prj/Gioco/Personaggio.java
+++ src/prj/Gioco/Personaggio.java
@@ -14,5 +14,79 @@ public class Personaggio {
     private int atk;
     private int def;
     private double range;
+    private int[] pos;
+
+    public Personaggio(String nome, int hp, int atk, int def, double range, int[] pos) {
+        this.nome = nome;
+        this.hp = hp;
+        this.atk = atk;
+        this.def = def;
+        this.range = range;
+        this.pos = pos;
+    }
+    
+    private boolean isVivo(int puntiVita){
+        boolean vivo= true;
+        if(puntiVita<1){
+            vivo=false;
+        }
+        
+        return vivo;
+    }
+    
+    public int attacco(Personaggio nemico){
+        String txt;
+        int danno= atk*3-def*2;
+        if(danno<1){
+            txt="hai mancato il colpo";
+        }
+        else{
+            nemico.hp-=danno;
+            boolean vivo=isVivo(nemico.hp);
+            if(vivo==true){
+                txt="il danno che ha subito "+nemico.nome+" è di "+danno+"\n la sua vita attuale è: "+nemico.hp;    
+            }
+            else{
+                txt="il nemico : "+nemico.nome+" è morto";
+            }
+            
+           
+        }
+    }
+    
+    public String attacco(Arma arma,Personaggio nemico){
+        String txt;
+        int danno= atk*3-def*2;
+        if(danno<1){
+            txt="hai mancato il colpo";
+        }
+        else{
+            nemico.hp-=danno;
+            boolean vivo=isVivo(nemico.hp);
+            if(vivo==true){
+                txt="il danno che ha subito "+nemico.nome+" è di "+danno+"\n la sua vita attuale è: "+nemico.hp;    
+            }
+            else{
+                txt="il nemico : "+nemico.nome+" è morto";
+            }
+        }
+        return txt;
+    }
+    
+    
+    
+
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
     
 }
